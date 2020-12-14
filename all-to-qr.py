b9ff073e19a035bcc69aa43171196b29d68784cc
vladislav <ve@incsub.com>
2020-12-14 19:28:16 +0100

    Add input parameter

--- all-to-qr.py
+++ all-to-qr.py
@@ -4,11 +4,12 @@ import sys, re
 import qrcode
 
 parser = ArgumentParser()
+parser.add_argument('input', metavar='<STRING>', help='Optional string to enter', nargs='?', type=str)
 parser.add_argument('-o', '--output', help="Output QR code, use dash for stdout, dot to use simplified data as file name")
 
 options = parser.parse_args()
 
-data = "".join(sys.stdin.readlines())
+data = options.input if options.input else "".join(sys.stdin.readlines())
 img = qrcode.make(data)
 
 output = sys.stdout.buffer
